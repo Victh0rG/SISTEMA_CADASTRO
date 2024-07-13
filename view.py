425f82f362e754a588b9ada6daeabfb1e2151873
Victhor Gabriel <victhorgb1080@gmail.com>
2024-07-13 10:01:22 -0300

    Criado CRUD para a tabela alunos

--- view.py
+++ view.py
@@ -27,7 +27,7 @@ def ver_cursos():
     with con:
         cur = con.cursor()
         cur.execute('SELECT * FROM cursos')
-        linha = cur.fetchall()
+        linha = cur.fetchall()  # Busca todas as linhas do resultado da consulta e atribui a lista 
 
         for i in linha:
             lista.append(i)
@@ -81,11 +81,12 @@ def ler_turmas():
 
 
 # Atualizar turmas (UPDATE)
-def atualizar_turmsa(i):
+def atualizar_turmas(i):
         with con:
             cur = con.cursor()
             query = "UPDATE turma SET nome=?, cursos_nome=?, data_inicio=? WHERE id=?"
             cur.execute(query,i)
+
 
 # Deletar turmas (DELETE)
 def deletar_turma(i):
@@ -93,3 +94,43 @@ def deletar_turma(i):
         cur = con.cursor()
         query = "DELETE  FROM turmas WHERE id=? "
         cur.execute(query,i)
+
+
+# TABELA DE CURSOS -------------------------------------------------------
+
+# Criar alunos (CREATE)
+def criar_alunos(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO alunos(nome, emial, telefone, sexo, imagem, data_nascimento, cpf, turma_nome ) VALUES(?,?,?,?,?,?,?)"
+        cur.execute(query, i)
+
+
+# Ler alunos (READE)
+def ler_alunos():
+    lista = []
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM alunos")
+        cur.fetchall()
+
+        for i in lista:
+            lista.append(i)
+    return lista
+
+
+# Atualizar alunos (UPDATE)
+def update_alunos(i):
+    with con:
+        cur = con.cursor()
+        query = "UPDATE alunos SET nome=?, emial=?, telefone=?, sexo=?, imagem=?, data_nascimento=?, cpf=?, turma_nome=? WHERE id=? "
+        cur.execute(query, i)
+
+
+# Deletar alunos (DELETE)
+def deletar_alunos(i):
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM alunos WHERE id=?"
+        cur.execute(query, i)
+
