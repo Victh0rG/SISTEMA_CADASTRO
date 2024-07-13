1fca9cf382874fd6b80cb2306a6d83a264569756
Victhor <victhorgb1080@gmail.com>
2024-07-12 21:12:06 -0300

    criando tabela curso

--- createdb.py
+++ createdb.py
@@ -1,9 +1,26 @@
 import sqlite3
 
 try:
+    #cria banco de dados
     con = sqlite3.connect('cadastro_alunos.db')
     print('conexao com o banco de dados realizada com sucesso!')
 
 except sqlite3.Error as e:
     print("Erro ao conectar com o banco de dados", e)
 
+#criando tabela para cursos
+try:
+    with con:
+        #criando variavel para instrucoes SQL
+        cur = con.cursor()
+        cur.execute(""" CREATE TABLE IF NOT EXISTS cursos(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT,
+                    duracoa TEXT,
+                    preco REAL)
+            """)
+        
+        print("tabela cursos criado com sucesso!")
+
+except sqlite3.Error as e:
+    print("Erro ao criar a tabela cursos", e)
