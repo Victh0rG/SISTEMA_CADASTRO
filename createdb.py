eae4d21af9b1b259470d5b9ffd5c0dac27dfdbcc
Victhor <victhorgb1080@gmail.com>
2024-07-12 21:02:42 -0300

    conexao com banco de dados

--- createdb.py
+++ createdb.py
@@ -1,1 +1,9 @@
-print("hello word")
+import sqlite3
+
+try:
+    con = sqlite3.connect('cadastro_alunos.db')
+    print('conexao com o banco de dados realizada com sucesso!')
+
+except sqlite3.Error as e:
+    print("Erro ao conectar com o banco de dados", e)
+
